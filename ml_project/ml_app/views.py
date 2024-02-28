a6176afc8e4cf9d9c3b1237aade960f9f31f2f5b
Viddesh1 <viddeshkagithub@gmail.com>
2024-02-28 15:47:38 +0530

    Images and videos can be pre-processed and post processed correctly

--- ml_project/ml_app/views.py
+++ ml_project/ml_app/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from . forms import ImageForm
 from . models import Image, PredImage
-from . utils import predict_yolo_image
+from . utils import predict_yolo_image, upload_image_video, image_video_path
 import os
 from django.core.files import File
-import time
+import shutil
 
 # Create your views here.
-
 def  index(request):
     # Getting all the uploaded images
     all_images = Image.objects.all()
@@ -29,11 +28,25 @@ def  index(request):
             #     image_instance.save()
 
             image_instance = form.save()
-            image_path = os.path.join("media", "images", "bike_rider.jpg")
-            predict_yolo_image(image_path)
-            video_path = os.path.join("media", "videos", "he2.mp4")
+
+            # Getting the newest file present in media/images directory.
+            newest_user_image = upload_image_video(dir_name = "images")
+
+            image_path = os.path.join("media", "images", newest_user_image)
+            predict_yolo_image(image_path) 
+
+            newest_directory, newest_file = image_video_path()
+            
+            newest_user_video = upload_image_video(dir_name = "videos")
+            video_path = os.path.join("media", "videos", newest_user_video)
             predict_yolo_image(video_path)
 
+            newest_video_directory, newest_video_file = image_video_path()
+
+            
+            
+            
+            #_________________------------------------____________
             # image_instance  = Image(
             #     image = os.path.join("runs", "detect", "predict", "bike_rider.jpg"), 
             #     video = os.path.join("runs", "detect", "predict", "bike_rider.jpg"), 
@@ -53,21 +66,26 @@ def  index(request):
             pred_image_instance = PredImage(pred_image_id=image_instance)
 
             # Open the image file and assign it to the pred_image field
-            pred_image_path = os.path.join("runs", "detect", "predict", "bike_rider.jpg")
+            pred_image_path = os.path.join("runs", "detect", newest_directory, newest_file)
             with open(pred_image_path, 'rb') as pred_image_file:
                 pred_image_instance.pred_image.save(os.path.basename(pred_image_path), File(pred_image_file))
 
             
             # time.sleep(2)
-            pred_video_path = os.path.join("runs", "detect", "predict2", "he2.avi")
+            pred_video_path = os.path.join("runs", "detect", newest_video_directory, newest_video_file)
             with open(pred_video_path, 'rb') as pred_video_file:
                 pred_image_instance.pred_video.save(os.path.basename(pred_video_path), File(pred_video_file))
 
             # Save the PredImage instance
             pred_image_instance.save()
+
+            # Deleting all the predicted images and videos by YOLO model to save memory.
+            shutil.rmtree(path = os.path.join("runs", "detect"))
             
     else:
         form = ImageForm()
     
     return render(request, 'ml_app/index.html', {'form': form, 'all_images': all_images, 'all_predictions': all_predictions}) 
+
+
 
