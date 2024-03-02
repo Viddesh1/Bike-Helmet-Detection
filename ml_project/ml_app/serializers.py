acb0edbcccae8210a25e5ca06fdc7a2c93b302c0
Viddesh1 <viddeshkagithub@gmail.com>
2024-03-02 22:43:12 +0530

    Updated serializer to support new data from end user

--- ml_project/ml_app/serializers.py
+++ ml_project/ml_app/serializers.py
@@ -6,6 +6,15 @@ class ImageSerializer(serializers.ModelSerializer):
         model = Image
         fields = '__all__'
 
+    def create(self, validated_data):
+        return Image.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.image = validated_data.get('image', instance.image)
+        instance.video = validated_data.get('video', instance.video)
+        instance.save()
+        return instance
+
 class PredImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = PredImage
