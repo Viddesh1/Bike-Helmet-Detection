e2336754ea8731eb3f9dbf932f11d2f89aafca3c
Viddesh1 <viddeshkagithub@gmail.com>
2024-03-16 21:37:48 +0530

    Updated for removal of knox

--- ml_project/ml_app/urls.py
+++ ml_project/ml_app/urls.py
@@ -7,21 +7,14 @@ from rest_framework import routers
 from .views import ImageViewSet, PredImageViewSet
 
 
-from knox.views import LoginView as KnoxLoginView
-from rest_framework import permissions
-
-
 router = routers.DefaultRouter()
 router.register(r'images', ImageViewSet)
 router.register(r'predimages', PredImageViewSet)
 
-class LoginView(KnoxLoginView):
-    permission_classes = (permissions.AllowAny,)  # Allow any user to login
 
 urlpatterns = [
     path("", views.index, name="index"),
     path('api/', include(router.urls)),
-    path('api/auth/login/', LoginView.as_view(), name='knox_login'),
 ]
 
 if settings.DEBUG:
