9a73db3a17be1edce78e9cdaf8491ff285f84252
Viddesh1 <viddeshkagithub@gmail.com>
2024-03-16 21:39:48 +0530

    Updated for the removal of knox

--- ml_project/ml_project/urls.py
+++ ml_project/ml_project/urls.py
@@ -17,13 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from knox import views as knox_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("ml_app.urls")),
-    path('api-auth/', include('rest_framework.urls'), name='rest_framework'),
-     path('api/auth/', include('knox.urls')),
+    # path('api-auth/', include('rest_framework.urls'), name='rest_framework'),
 ]
 
 
