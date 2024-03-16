fe496a41c4e672f76cf8b8435d485456c1bc6165
Viddesh1 <viddeshkagithub@gmail.com>
2024-03-16 22:03:18 +0530

    Added TODO

--- ml_project/ml_app/forms.py
+++ ml_project/ml_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from . models import Image
 
+# TODO: Add validations to the form data and test case for it.
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
