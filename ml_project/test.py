92a3b56bff505468afa4e40c406602686f42ccb8
Viddesh1 <viddeshkagithub@gmail.com>
2024-03-16 22:02:42 +0530

    Deleting the test.py file as the pre-building testing is done

--- ml_project/test.py
+++
@@ -1,7 +0,0 @@
-# from ultralytics import YOLO
-
-# Load a pretrained YOLOv8n model
-# model = YOLO('c:/testing/weights/best.pt')
-
-# # Run inference on 'bus.jpg' with arguments
-# model.predict('c:/testing/BikesHelmets6.png', save=True, imgsz=320, conf=0.5)
