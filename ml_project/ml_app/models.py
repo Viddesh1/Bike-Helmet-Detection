8175684eb1e046d9976778aa418369c7c727460b
Viddesh1 <viddeshkagithub@gmail.com>
2024-03-17 21:00:12 +0530

    Updated data values fields to blank = False and null = True

--- ml_project/ml_app/models.py
+++ ml_project/ml_app/models.py
@@ -4,8 +4,8 @@ import os
 # Create your models here.
 class Image(models.Model):
     id = models.AutoField(primary_key = True)
-    image = models.ImageField(upload_to="images", blank = True, null = True)
-    video = models.FileField(upload_to = "videos", blank = True, null = True)
+    image = models.ImageField(upload_to="images", blank = False, null = True)
+    video = models.FileField(upload_to = "videos", blank = False, null = True)
     
 
     def __str__(self):
@@ -14,8 +14,8 @@ class Image(models.Model):
 
 class PredImage(models.Model):
     pred_image_id = models.OneToOneField(Image, primary_key = True, on_delete = models.CASCADE)
-    pred_image = models.ImageField(upload_to="pred_images", blank=True, null=True)
-    pred_video = models.FileField(upload_to="pred_videos", blank=True, null=True)
+    pred_image = models.ImageField(upload_to="pred_images", blank=False, null=True)
+    pred_video = models.FileField(upload_to="pred_videos", blank=False, null=True)
 
     def __str__(self):
         return f"Prediction for Image {self.pred_image_id.id} - Pred_image: {self.pred_image} - Pred_video: {self.pred_video}"
