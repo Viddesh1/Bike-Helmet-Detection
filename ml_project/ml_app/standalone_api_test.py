11cdac6569d72e9fc56a5331e6a9952d5d082a2c
Viddesh1 <viddeshkagithub@gmail.com>
2024-03-19 17:38:15 +0530

    Updated for validation test -successful

--- ml_project/ml_app/standalone_api_test.py
+++ ml_project/ml_app/standalone_api_test.py
@@ -32,30 +32,39 @@ pred_image_url = f'{base_url}/api/predimages/'
 
 # # Send a POST request to create a new Image
 # response = requests.post(image_url, auth=(credentials.USERNAME, credentials.PASSWORD), files=image_data)
-# print('Image API Response:', response.status_code, response.json())
+# # print('Image API Response:', response.status_code, response.json())
 # json_str = json.dumps(response.json(), indent=4)
 # print(json_str)
 
-# Sample data for creating an only Image
-image_data = {
-    'image': open(os.path.join(os.getcwd(), "tests", "test_assets", "test_images", "bike_rider.jpg"), 'rb')
-}
-
-# Send a POST request to create a new Image
-response = requests.post(image_url, auth=(credentials.USERNAME, credentials.PASSWORD), files=image_data)
-print('Image API Response:', response.status_code, response.json())
-json_str = json.dumps(response.json(), indent=4)
-print(json_str)
-
-# # Sample data for testing null values. Should give error.
+# # Sample data for creating an only Image
 # image_data = {
-#     'image': [],
-#     'video': []
+#     'image': open(os.path.join(os.getcwd(), "tests", "test_assets", "test_images", "bike_rider.jpg"), 'rb')
 # }
 
 # # Send a POST request to create a new Image
 # response = requests.post(image_url, auth=(credentials.USERNAME, credentials.PASSWORD), files=image_data)
-# print('Image API Response:', response.status_code, response.json())
+# # print('Image API Response:', response.status_code, response.json())
+# json_str = json.dumps(response.json(), indent=4)
+# print(json_str)
+
+# # Sample data for testing video file only. Should give error.
+# image_data = {
+#     'video': open(os.path.join(os.getcwd(), "tests", "test_assets", "test_videos", 'he2.mp4'), 'rb')
+# }
+
+# # Send a POST request to create a new Image
+# response = requests.post(image_url, auth=(credentials.USERNAME, credentials.PASSWORD), files=image_data)
+# # print('Image API Response:', response.status_code, response.json())
+# json_str = json.dumps(response.json(), indent=4)
+# print(json_str)
+
+# # Sample data for testing null values. Should give error.
+# image_data = {
+# }
+
+# # Send a POST request to create a new Image
+# response = requests.post(image_url, auth=(credentials.USERNAME, credentials.PASSWORD), files=image_data)
+# # print('Image API Response:', response.status_code, response.json())
 # json_str = json.dumps(response.json(), indent=4)
 # print(json_str)
 
@@ -64,68 +73,12 @@ print(json_str)
 # json_str = json.dumps(response.json(), indent=4)
 # print(json_str)
 
-# GET the POST response that I just did.
-# response = requests.get(url = f'{base_url}/api/predimages/26/', auth=(credentials.USERNAME, credentials.PASSWORD))
-# print(f"Status code: {response.status_code}\n, {response.json()}")
+# # GET the POST response that I just did.
+# response = requests.get(url = f'{base_url}/api/predimages/43/', auth=(credentials.USERNAME, credentials.PASSWORD))
+# print(f"Status code: {response.status_code}")
 # json_str = json.dumps(response.json(), indent=4)
 # print(json_str)
 
-# DELETE response should give error.
+# # DELETE response should give functionality not found error.
 # response = requests.delete(url = f'{base_url}/api/predimages/25/', auth=(credentials.USERNAME, credentials.PASSWORD))
 # print(f"Status code: {response.status_code}\n, {response.json()}")
-
-
-
-####################################################
-
-# import requests
-# import os
-
-# # Base URL of your Django server
-# base_url = 'http://127.0.0.1:8000'
-
-# # Endpoint for the Image API
-# image_url = f'{base_url}/api/images/'
-
-# # Endpoint for the PredImage API
-# pred_image_url = f'{base_url}/api/predimages/'
-
-# # Send a POST request to create a new Image
-# response = requests.post(image_url, data={'username': 'viddesh', 'password': '!@#$%^&*()_+'}, files=image_data)
-# print('Image API Response:', response.status_code, response.json())
-
-# # Send a GET request to retrieve all Images
-# response = requests.get(image_url)
-# print('All Images API Response:', response.status_code, response.json())
-
-# # Send a GET request to retrieve a specific Image by ID
-# response = requests.get(f'{image_url}1/')
-# print('Single Image API Response:', response.status_code, response.json())
-
-# # Send a PUT request to update an Image by ID
-# response = requests.put(f'{image_url}1/', files=image_data)
-# print('Update Image API Response:', response.status_code, response.json())
-
-# # Send a DELETE request to delete an Image by ID
-# response = requests.delete(f'{image_url}1/')
-# print('Delete Image API Response:', response.status_code, response.json())
-
-# # Send a POST request to create a new PredImage
-# response = requests.post(pred_image_url, data={'pred_image_id': 1})
-# print('PredImage API Response:', response.status_code, response.json())
-
-# # Send a GET request to retrieve all PredImages
-# response = requests.get(pred_image_url)
-# print('All PredImages API Response:', response.status_code, response.json())
-
-# # Send a GET request to retrieve a specific PredImage by ID
-# response = requests.get(f'{pred_image_url}1/')
-# print('Single PredImage API Response:', response.status_code, response.json())
-
-# # Send a PUT request to update a PredImage by ID
-# response = requests.put(f'{pred_image_url}1/', data={'pred_image_id': 2})
-# print('Update PredImage API Response:', response.status_code, response.json())
-
-# # Send a DELETE request to delete a PredImage by ID
-# response = requests.delete(f'{pred_image_url}1/')
-# print('Delete PredImage API Response:', response.status_code, response.json())
