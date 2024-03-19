68eaa9059338bb9972d60b4c0378664cf7cb3115
Viddesh1 <viddeshkagithub@gmail.com>
2024-03-19 17:39:56 +0530

    Removed unwanted spaces

--- ml_project/ml_app/utils.py
+++ ml_project/ml_app/utils.py
@@ -25,16 +25,10 @@ def upload_image_video(dir_name):
 
 def image_video_path():
     src_path = os.path.join("runs", "detect")
-    # dst_path = os.path.join("dst")
-
     # Find the newest directory in src
     newest_directory = max([d for d in os.listdir(src_path) if os.path.isdir(os.path.join(src_path, d))], key=lambda x: os.path.getctime(os.path.join(src_path, x)))
-
     # Path to the newest directory
     newest_directory_path = os.path.join(src_path, newest_directory)
-
     # Find the newest file inside the newest directory
     newest_file = max([f for f in os.listdir(newest_directory_path) if os.path.isfile(os.path.join(newest_directory_path, f))], key=lambda x: os.path.getctime(os.path.join(newest_directory_path, x)))
-
-    
     return newest_directory, newest_file
