c21ea0f6f69ba6a73a16f991655759226377b5bd
Viddesh1 <viddeshkagithub@gmail.com>
2024-04-08 21:06:54 +0530

    Added doc strings and informatory comments

--- ml_project/ml_app/web_cam.py
+++ ml_project/ml_app/web_cam.py
@@ -1,5 +1,4 @@
 from ultralytics import YOLO
-# from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
 
 model = YOLO("weights/best.pt")
@@ -8,9 +7,9 @@ model = YOLO("weights/best.pt")
 # This code will open webcam to run YOLOv8 inference upon
 # Highly dependent upon computational speed and camera quality fps too!.
 result = model.predict(source = "0", stream=True, show=True) # save=True, imgsz=320, conf=0.5
-# print(result)
+
 for r in result:
     boxes = r.boxes  # Boxes object for bbox outputs
     masks = r.masks  # Masks object for segment masks outputs
     probs = r.probs  # Class probabilities for classification outputs
-    # print(r)
+
